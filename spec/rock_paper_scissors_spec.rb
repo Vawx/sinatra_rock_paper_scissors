5e60e61d60d9481023c3de01bf74bebc1c70d99b
Kyle Langley and Lauryn Davis <lryndavis@gmail.com>
2016-01-06 13:18:27 -0800

    passes other sinatra specs

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -5,4 +5,10 @@ describe("Fixnum#rock_paper_scissors") do
   it('will return rock when user input is 0') do
     expect( 0.rock_paper_scissors ).to(eq("rock"))
   end
+  it('will return paper when user input is 1') do
+    expect( 1.rock_paper_scissors ).to(eq("paper"))
+  end
+  it('will return scissors when user input is 2') do
+    expect( 2.rock_paper_scissors ).to(eq("scissors"))
+  end
 end
